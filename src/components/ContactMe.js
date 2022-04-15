90f870ce2409dea0e88aa5cbae6dd10dacd752a6
Annika Neumann <annika1983neumann@gmail.com>
2022-04-15 15:18:11 +0100

    setting up components

---
+++ src/components/ContactMe.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default ContactMe;
---
+++ src/container/PortfolioContainer.js
@@ -0,0 +1,33 @@
+import {useState, useEffect} from 'react';
+
+import styled from 'styled-components';
+
+
+const Title = styled.h1`
+text-align: center; 
+  font-size: 1.5em;
+  color: #fff;
+  background-color: #062c43;
+  margin: 0;
+  padding: 1em;
+`
+
+
+const PortfolioContainer = () =>{
+
+    const[portfolio, setPortfolio] = useState[nul]
+
+
+
+
+    return(
+        <>    
+        <Title>WELCOME TO MY PORTFOLIO</Title>
+        <h2>Planned and designed using Javascript, CSS and React</h2>
+        </>
+
+    )
+}
+
+
+export default PortfolioContainer;
