8ec187004f16f3067348f598787c587ee55d48ce
Annika Neumann <annika1983neumann@gmail.com>
2022-04-15 14:16:06 +0100

    Updates

---
+++ precache-manifest.787f765639f67322ba8ad8190cac22f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5388dfb244ae52946666",
+    "url": "/static/css/main.fdcd9bed.chunk.css"
+  },
+  {
+    "revision": "5388dfb244ae52946666",
+    "url": "/static/js/main.5388dfb2.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "413b728f6bda178a29fd",
+    "url": "/static/js/2.413b728f.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7b4943081252931acac3f539178e7a06",
+    "url": "/index.html"
+  }
+];
