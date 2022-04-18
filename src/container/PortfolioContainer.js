4813c2c0b825524712d3a71901ee420d8b580797
Annika Neumann <annika1983neumann@gmail.com>
2022-04-18 08:19:22 +0100

    changed About text

--- src/container/PortfolioContainer.js
+++ src/container/PortfolioContainer.js
@@ -3,7 +3,6 @@ import '../App.css';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
 import About from '../components/About';
-import Footer from '../components/Footer';
 
  
 
