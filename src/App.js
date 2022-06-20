c2b023e8731d10306624393dc9d174bccbd4b920
Annika83 <annika1983neumann@gmail.com>
2022-06-20 14:20:54 +0100

    starting to make it more responsive and mobile phone friendly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, { useState, useContext } from 'react';
 import PortfolioContainer from './container/PortfolioContainer';
+{/* <meta name="viewport" content="width=device-width, initial-scale=1.0"> */}
+
 
 function App() {
 
