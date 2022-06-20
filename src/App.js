e6c41070c6d48f38c0d66553b08120e44ddddb4c
Annika83 <annika1983neumann@gmail.com>
2022-06-20 20:29:49 +0100

    trial and error with layout being responsive, to be continued

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useState, useContext } from 'react';
 import PortfolioContainer from './container/PortfolioContainer';
-{/* <meta name="viewport" content="width=device-width, initial-scale=1.0"> */}
 
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,11 +9,11 @@ const Header = () => {
             <nav>
                 <ul>
                     <li>
-                    <a href="https://github.com/AnnikaNeumann" target="_blank"><img className="github" src={require("../images/github-sign.png")} alt='Github'/></a>
+                    <a href="https://github.com/AnnikaNeumann" target="_blank" rel="noreferrer"><img className="github" src={require("../images/github-sign.png")} alt='Github link'/></a>
                     </li>
                     
                     <li>
-                    <a href="https://www.linkedin.com/in/annika-neumann-422b98232/" target="_blank"><img className="linkedin" src={require("../images/linkedin.png")} alt='LinkedIn'/></a>
+                    <a href="https://www.linkedin.com/in/annika-neumann-422b98232/" target="_blank" rel="noreferrer"><img className="linkedin" src={require("../images/linkedin.png")} alt='LinkedIn link'/></a>
                     </li>
                     <li>
                     <h1>Annika Neumann<br></br>Life between Coding and Cats</h1>
